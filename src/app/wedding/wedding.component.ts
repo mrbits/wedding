4281ef06e1068e130c5a3171266ded12bf12302d
mrbits <lucasjschmidt@gmail.com>
2017-06-23 16:21:00 -0500

    title and favicon

--- src/app/wedding/wedding.component.ts
+++ src/app/wedding/wedding.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-wedding',
@@ -7,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WeddingComponent implements OnInit {
 
-  constructor() { }
+  constructor(private titleService: Title ) { }
 
   ngOnInit() {
+    this.titleService.setTitle('Amanda & Luke\'s Wedding');
   }
-
 }
